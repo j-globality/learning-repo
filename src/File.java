2cfd6d08ff4ca4ac9f88713b5a05e631bdbc6ff2
Jon <jon.abdulloev@globality.com>
2022-10-21 16:57:05 +0100

    WIP methods

---
+++ src/File.java
@@ -0,0 +1,20 @@
+/*
+* Create a file
+* Writing content to it
+* Read from file
+* */
+public class File {
+
+    public void createFile() {
+        
+    }
+
+    public void writeToFile() {
+
+    }
+
+    public void readFromFile() {
+
+    }
+
+}
